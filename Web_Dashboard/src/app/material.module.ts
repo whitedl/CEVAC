495fbffb583b13fc736aa113ac50e5f824a61903
ZTKlein <ztklein@clemson.edu>
2019-05-31 11:13:15 -0400

    More updating. Changed code where needed.

--- Web_Dashboard/src/app/material.module.ts
+++ Web_Dashboard/src/app/material.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
 
-import {
-	MatSidenavModule,
-	MatToolbarModule,
-	MatIconModule,
-	MatListModule,
-	MatButtonModule,
-	MatBadgeModule,
-	MatExpansionModule
-} from '@angular/material';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
 	imports: [
