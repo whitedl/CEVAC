a0efcaf41a6597a5df6977d23dc81c565c79cd56
Andrew <abemery@clemson.edu>
2019-06-03 16:00:08 -0400

    I thought I was moving clear of murky waters but I seem to have been propelled ever deeper. Good things to come

--- prediction/kpa.py
+++ prediction/kpa.py
@@ -9,6 +9,10 @@ from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 from tensorflow import keras
 from keras import losses
+from keras.utils import plot_model
+
+# local libs
+import dataSplitter
 
 # Helper libraries
 import numpy as np
@@ -20,42 +24,69 @@ import scipy
 import datetime
 import os
 
-# classNames is the number of people that we believe are on the wifi
-classNames = []
+def vis():
+	# get out x and y values
+	x, y = dataSplitter.theLists()
 
-# the capacity of the building
-capacity = 500
+	df = dataSplitter.formatDate()
 
-for i in range(0,capacity):
-	classNames.append(i)	#these are your possible outputs (number of devices on the wifi
+	# make our correlation plot
+	corr = df.corr()
 
-train_data = np.load("trainingData.npy")
-train_labels = np.load("trainingLabels.npy")
+	sns.heatmap(corr,
+            xticklabels=corr.columns.values,
+            yticklabels=corr.columns.values)
+	plt.show()
 
-test_data = np.load("testingData.npy")
-test_labels = np.load("testingLabels.npy")
+def loadData():
+	train_data = np.load("trainingData.npy")
+	train_labels = np.load("trainingLabels.npy")
 
-train_data = train_data / train_data.max()
-test_data = test_data / test_data.max()
+	test_data = np.load("testingData.npy")
+	test_labels = np.load("testingLabels.npy")
+
+	train_data = train_data / train_data.max()
+	test_data = test_data / test_data.max()
+
+	return train_data, train_labels, test_data, test_labels
 
 
-model = keras.Sequential([
-	#keras.layers.Flatten(input_shape=(NUM_ATTRIBUTES,1)), you probably don't need this line bc your input data for each hour is already flat (aka one of your dims is 1)
-	keras.layers.Dense(64, activation=tf.nn.relu), #64 was basically a random number for me. I'd experiment with bigger and smaller
-	keras.layers.Dropout(0.3),		#makes sure you aren't overfitting and killing your test accuracy. This is a pretty high dropout rate, so make lower as needed (esp if you need a bigger training set)
-	keras.layers.Dense(capacity + 1, activation=tf.nn.relu) #number of neurons in final layer should equal number of classes
-])
+def predAlg():
 
-# if changes are going to be made to increase accuracy it should be done here
-model.compile(optimizer='sgd',loss=losses.mean_squared_error, metrics=['accuracy'])
+	# load data
+	train_data, train_labels, test_data, test_labels = loadData()
 
-#more epochs = more work training ~= higher accuracy
-model.fit(train_data, train_labels, epochs=10000, verbose=1)
+	# classNames is the number of people that we believe are on the wifi
+	classNames = []
 
-#for making re-running faster, toggle this to re-run with the same weights from the previous run
-model.save_weights("./checkpoints/weights")
-#model.load_weights works in reverse)
+	# the capacity of the building
+	capacity = 50
 
-test_loss, test_acc = model.evaluate(test_data, test_labels)
+	for i in range(0,capacity):
+		classNames.append(i)	#these are your possible outputs (number of devices on the wifi
 
-print('Test accuracy:', test_acc)
+	model = keras.Sequential([
+		#keras.layers.Flatten(input_shape=(NUM_ATTRIBUTES,1)), you probably don't need this line bc your input data for each hour is already flat (aka one of your dims is 1)
+		keras.layers.Dense(64, activation=tf.nn.relu, input_shape=(5,)), #64 was basically a random number for me. I'd experiment with bigger and smaller
+		keras.layers.Dropout(0.1),		#makes sure you aren't overfitting and killing your test accuracy. This is a pretty high dropout rate, so make lower as needed (esp if you need a bigger training set)
+		keras.layers.Dense(capacity + 1, activation=tf.nn.relu) #number of neurons in final layer should equal number of classes
+	])
+
+	# if changes are going to be made to increase accuracy it should be done here
+	model.compile(optimizer='adam',loss=losses.mean_squared_error, metrics=['accuracy'])
+
+	#more epochs = more work training ~= higher accuracy
+	model.fit(train_data, train_labels, epochs=100, verbose=1)
+
+	#for making re-running faster, toggle this to re-run with the same weights from the previous run
+	model.save_weights("./checkpoints/weights")
+	#model.load_weights works in reverse)
+
+	# plot_model(model, to_file='model.png')
+
+	test_loss, test_acc = model.evaluate(test_data, test_labels)
+
+	print('Test accuracy:', test_acc)
+
+if __name__ == '__main__':
+	vis()
