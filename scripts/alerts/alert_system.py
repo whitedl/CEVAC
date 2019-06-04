31ee8ea31a8cb1c453ac64c9e54029e353520d25
Harrison Hall <harrisonchristianhall@gmail.com>
2019-06-04 16:26:50 -0400

    BAREBONES alert system ready, a relative alert system is almost ready

--- scripts/alerts/alert_system.py
+++ scripts/alerts/alert_system.py
@@ -1,3 +1,7 @@
+# alert_system.py
+
+### TODO: special
+
 import os, sys
 import csv
 import pypyodbc
@@ -8,7 +12,7 @@ import pytz
 import csv
 import logging
 
-CONDITIONS_FPATH = "C:\\Users\\hchall\\Downloads\\Alert Parameters (working).csv"
+CONDITIONS_FPATH = "C:\\Users\\hchall\\Downloads\\"
 LOGGING_PATH = "C:\\Users\\hchall\\Documents\\GitHub\\CEVAC\\scripts\\alerts\\logging\\"
 PHONE_PATH = "C:\\Users\\hchall\\Documents\\GitHub\\CEVAC\\scripts\\alerts\\"
 CONFIG_PATH = "//130.127.219.170/Watt/Watt Staff/Building/WAP/config/"
@@ -16,58 +20,82 @@ CONFIG_PATH = "//130.127.219.170/Watt/Watt Staff/Building/WAP/config/"
 COLUMNS = {
 	"alert_name" : 0,
 	"type" : 1,
-	"unit" : 2,
-	"message" : 3,
-	"building" : 4,
-	"database" : 5,
-	"column" : 6,
-	"sort_column" : 7,
-	"num_entries" : 8,
-	"hour" : 9,
-	"day" : 10,
-	"month" : 11,
-	"condition" : 12,
-	"value" : 13,
-	"operation" : 14,
+	"aliases" : 2,
+	"unit" : 3,
+	"message" : 4,
+	"building" : 5,
+	"database" : 6,
+	"column" : 7,
+	"sort_column" : 8,
+	"num_entries" : 9,
+	"hour" : 10,
+	"day" : 11,
+	"month" : 12,
+	"condition" : 13,
+	"value" : 14,
+	"operation" : 15,
 }
 
 
 
 # Definitions
 
-def regex_to_list(regex_string):
+def regex_to_numlist(regex_string):
+	"""
+	Returns a list of numbers following expressions similar to "1-5 & 9-10" to [1,2,3,4,5,9,10]
+	"""
 	if regex_string == "*":
 		return regex_string
 	else:
-		regex_list = [([int(y)] if len(y.split("-")) == 1 else list(range(int(y.split("-")[0]),int(y.split("-")[1])+1))) for y in regex_string.split("&")]
+		regex_list = [([int(y)] if len(y.split("-")) == 1 else list(range(int(y.split("-")[0]),int(y.split("-")[1])+1))) for y in regex_string.replace(" ","").split("&")]
 		return_list = []
 		for num_list in regex_list:
 			for num in num_list:
 				return_list.append(num)
 		return return_list
 
-def import_conditions(fname):
+def alias_to_list(regex_string):
+	"""
+	Returns a list of strings that were originally seperated by hyphens
+	"""
+	if regex_string == "*":
+		return "*"
+	return regex_string.split("-")
+
+def angle_brackets_replace(regex_string,alert):
+	try:
+		regex_list = regex_string.replace("<","").split(">")
+		for regex in regex_list:
+			if regex == "" #TODO
+	except:
+		return regex_string
+
+
+def import_conditions(fname,logger):
 	alerts = {}
-	with csv.reader(open(CONDITIONS_FPATH+fname)) as csvfile:
+	with open(CONDITIONS_FPATH+fname) as csvfile:
+		csvfile = csv.reader(csvfile)
 		next(csvfile)
 		for i,row in enumerate(csvfile):
 			try:
-				if (i > 0):
-					alerts[COLUMN["alert_name"]] = {
+				if (i >= 0):
+					alerts[row[COLUMNS["alert_name"]]] = {
 						"type" : row[COLUMNS["type"]],
 						"message" : row[COLUMNS["message"]],
 						"database" : row[COLUMNS["database"]],
-						"column" : int(row[COLUMNS["column"]]),
+						"column" : row[COLUMNS["column"]],
 						"num_entries" : int(row[COLUMNS["num_entries"]]),
-						"hour" : list(range(24)) if regex_to_list(row[COLUMNS["hour"]]), #EST
-						"day" : list(range(24)) if regex_to_list(row[COLUMNS["day"]]), #EST
-						"month" : list(range(24)) if regex_to_list(row[COLUMNS["month"]]), #EST
+						"hour" : regex_to_numlist(row[COLUMNS["hour"]]), #EST
+						"day" : regex_to_numlist(row[COLUMNS["day"]]), #EST
+						"month" : regex_to_numlist(row[COLUMNS["month"]]), #EST
 						"condition" : row[COLUMNS["condition"]],
-						"value" : int(row[COLUMNS["value"]]),
+						"value" : float(row[COLUMNS["value"]]),
 						"operation" : row[COLUMNS["operation"]],
+						"aliases" : alias_to_list(row[COLUMNS["aliases"]]),
+						"sort_column" : row[COLUMNS["sort_column"]],
 					}
 			except:
-				pass #LOG TODO
+				logger.error("Issue importing conditions "+str(i))
 	return alerts
 
 def get_config(fname):
@@ -89,6 +117,7 @@ def send_email_list(email_address_list,content):
 # Script
 
 ## Initialize logging
+FORMAT = '%(asctime)s %(levelname)s:%(message)s'
 datestring = str(datetime.datetime.now().date())
 log_file = os.path.join(LOGGING_PATH, datestring + '.log')
 logging.basicConfig(filename=log_file, format=FORMAT, level=logging.INFO)
@@ -99,7 +128,7 @@ emails = [email.replace("\n","") for email in open(PHONE_PATH+fname)]
 
 ## Get alert conditions
 fname = "Alert Parameters (Working).csv"
-alerts = import_conditions(fname)
+alerts = import_conditions(fname,logging)
 
 ## Connect to database
 fname = "dbconfig2.json"
@@ -113,28 +142,36 @@ connection = pypyodbc.connect(
 cursor = connection.cursor()
 
 ## Check alerts
-for alert in import_conditions(CONDITIONS_FPATH):
-	for i,alert in enumerate(alerts):
-		try:
-			now = datetime.datetime.now()
-			if (now.isoweekday() in alert["day"]) and (now.hour in alert["hour"]) and (now.month in alert["month"]):
-				selection_command = "SELECT top "+str(alert["num_entries"]) + " " + alerts["column"] + " FROM " + str(alerts["database"] + " ORDER BY " + alerts["sort"] + " RowNum DESC"
-				data = cursor.execute(selection_command)
-				data_list = [row[0] for row in data]
-				avg_data = sum(data_list)/len(data_list)
-				send_alert = False
-				if alert["condition"] == ">":
-					send_alert = (avg_data > alert["value"])
-				elif alert["condition"] == "<":
-					send_alert = (avg_data < alert["value"])
-				if send_alert:
-					## TODO check operation
-					insert_sql = "INSERT INTO CEVAC_ALL_ALERTS_HIST(AlertMessage,AlertType, Metric,BLDG,BeginTime)VALUES(?, ?,?,?,GETUTCDATE())"
-					cursor.execute(insert_sql, [alert["message"],alert["type"],avg_data,alert["building"]])
-				print("checked",alert,"\n\n")
-		except:
-			print("Issue on alert",i,"\n",alert,"\n\n")
-
+for i,a in enumerate(alerts):
+	alert = alerts[a]
+	try:
+		now = datetime.datetime.now()
+		if (((str(now.isoweekday()) in alert["day"]) or (str(alert["day"]) == "*"))
+		and ((str(now.hour) in alert["hour"]) or (str(alert["hour"]) == "*"))
+		and ((str(now.month) in alert["month"]) or (str(alert["month"]) == "*"))):
+			selection_command = "SELECT top "+str(alert["num_entries"]) + " " + alert["column"] + " FROM " + str(alert["database"])
+			if str(alert["aliases"]) == "*":
+				selection_command += " ORDER BY " + alert["sort_column"] + " DESC"
+			else:
+				selection_command += " WHERE " + "Alias" + " IN (" + str(alert["aliases"]).replace("[","").replace("]","") + ") ORDER BY " + alert["sort_column"] + " DESC"
+			data = cursor.execute(selection_command)
+			data_list = [row[0] for row in data]
+			avg_data = sum(data_list)/len(data_list)
+			send_alert = False
+			if alert["condition"] == ">":
+				send_alert = (avg_data > alert["value"])
+			elif alert["condition"] == "<":
+				send_alert = (avg_data < alert["value"])
+			if send_alert:
+				insert_sql = "INSERT INTO CEVAC_ALL_ALERTS_HIST(AlertMessage,AlertType, Metric,BLDG,BeginTime) VALUES(?,?,?,?,GETUTCDATE())"
+				cursor.execute(insert_sql, [alert["message"],alert["operation"],avg_data,alert["building"]])
+				logging.info("An alert was sent for "+str(alert))
+			logging.info("Checked "+str(alert))
+		else:
+			#print("Not time for",alert)
+			pass
+	except:
+		logging.error("Issue on alert "+str(i)+" "+str(alert))
 
 
 logging.shutdown()
