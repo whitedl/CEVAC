b28cb3999656c96363b059484939632a1b9084c9
Andrew <abemery@clemson.edu>
2019-06-05 10:57:17 -0400

    categorical crossentropy seems to be the move; will be adding additional layers to the custom MLP to see if it improves accuracy

--- prediction/kpa.py
+++ prediction/kpa.py
@@ -1,15 +1,12 @@
-'''
-General note for your data - I'd ranndomly chunk it up so 90% is training data, and ~10% is test. The lowest you probably
-wanna go for test data is 5% of your original set. If you need more data, I'd just make up/generate some realistic cases.
-'''
-
 from __future__ import absolute_import, division, print_function
 
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
 from keras import losses
+from tensorflow.keras.callbacks import EarlyStopping
 from keras.utils import plot_model
+
 
 # local libs
 import dataSplitter
@@ -24,21 +21,33 @@ import scipy
 import datetime
 import os
 
-def vis():
-	# get out x and y values
-	x, y = dataSplitter.theLists()
+class SimpleMLP(keras.Model):
 
-	df = dataSplitter.formatDate()
+    def __init__(self, use_bn=False, use_dp=False, num_classes=500):
+        super(SimpleMLP, self).__init__(name='mlp')
+        self.use_bn = use_bn
+        self.use_dp = use_dp
+        self.num_classes = num_classes
 
-	# make our correlation plot
-	corr = df.corr()
+        self.dense1 = keras.layers.Dense(256, activation='relu')
+        self.dense2 = keras.layers.Dense(num_classes, activation='softmax')
+        if self.use_dp:
+            self.dp = keras.layers.Dropout(0.3)
+        if self.use_bn:
+            self.bn = keras.layers.BatchNormalization(axis=-1)
 
-	sns.heatmap(corr,
-            xticklabels=corr.columns.values,
-            yticklabels=corr.columns.values)
-	plt.show()
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        if self.use_dp:
+            x = self.dp(x)
+        if self.use_bn:
+            x = self.bn(x)
+        return self.dense2(x)
+
+
 
 def loadData():
+
 	train_data = np.load("trainingData.npy")
 	train_labels = np.load("trainingLabels.npy")
 
@@ -50,43 +59,66 @@ def loadData():
 
 	return train_data, train_labels, test_data, test_labels
 
+def customAlg():
 
-def predAlg():
+	# make a model instanace
+	model = SimpleMLP()
+	# stops the model when the loss is no longer decreasing
+	model.compile(optimizer='adam',loss=losses.categorical_crossentropy, metrics=['accuracy'])
+	return model
+
+
+def predAlg(buildingOccupancy):
+	# the capacity of the building
+	class_names = [i for i in range(0,buildingOccupancy)]	#these are your possible outputs (number of devices on the wifi
+
+	model = keras.Sequential([
+		keras.layers.Dense(256, activation=tf.nn.relu), #64 was basically a random number for me. I'd experiment with bigger and smaller
+		keras.layers.Dropout(0.25),		# makes sure you aren't overfitting and killing your test accuracy. This is a pretty high dropout rate, so make lower as needed (esp if you need a bigger training set)
+		keras.layers.Dense(buildingOccupancy, activation=tf.nn.relu) #number of neurons in final layer should equal number of classes
+	])
+
+	# if changes are going to be made to increase accuracy it should be done here
+	model.compile(optimizer='adam',loss=losses.categorical_crossentropy, metrics=['accuracy'])
+	return model
+
+def train(model):
 
 	# load data
 	train_data, train_labels, test_data, test_labels = loadData()
 
-	# classNames is the number of people that we believe are on the wifi
-	classNames = []
-
-	# the capacity of the building
-	capacity = 50
-
-	for i in range(0,capacity):
-		classNames.append(i)	#these are your possible outputs (number of devices on the wifi
-
-	model = keras.Sequential([
-		#keras.layers.Flatten(input_shape=(NUM_ATTRIBUTES,1)), you probably don't need this line bc your input data for each hour is already flat (aka one of your dims is 1)
-		keras.layers.Dense(64, activation=tf.nn.relu, input_shape=(5,)), #64 was basically a random number for me. I'd experiment with bigger and smaller
-		keras.layers.Dropout(0.1),		#makes sure you aren't overfitting and killing your test accuracy. This is a pretty high dropout rate, so make lower as needed (esp if you need a bigger training set)
-		keras.layers.Dense(capacity + 1, activation=tf.nn.relu) #number of neurons in final layer should equal number of classes
-	])
-
-	# if changes are going to be made to increase accuracy it should be done here
-	model.compile(optimizer='adam',loss=losses.mean_squared_error, metrics=['accuracy'])
+	# stops the model when the loss is no longer decreasing
+	early_stopping = EarlyStopping(monitor='loss', patience=50)
 
 	#more epochs = more work training ~= higher accuracy
-	model.fit(train_data, train_labels, epochs=100, verbose=1)
+	model.fit(train_data, train_labels, epochs=10000, verbose=0, callbacks=[early_stopping])
 
 	#for making re-running faster, toggle this to re-run with the same weights from the previous run
 	model.save_weights("./checkpoints/weights")
-	#model.load_weights works in reverse)
-
-	# plot_model(model, to_file='model.png')
+	# model.load_weights("./checkpoints/weights")	# works in reverse)
 
 	test_loss, test_acc = model.evaluate(test_data, test_labels)
 
 	print('Test accuracy:', test_acc)
 
+	# print('GUESS:	{}'.format(model.predict((325.0, 2.0, 9.0, 18.0))))
+	# print('GUESS:	{}'.format(model.predict((393.0, 4.0, 9.0, 18.0))))
+	# print('GUESS:	{}'.format(model.predict((119.0, 5.0, 9.0, 20.0))))
+
+def predict(buildingOccupancy):
+
+	model = keras.Sequential([
+		# keras.layers.Flatten(input_shape=(NUM_ATTRIBUTES,1)), # you probably don't need this line bc your input data for each hour is already flat (aka one of your dims is 1)
+		keras.layers.Dense(512, activation=tf.nn.relu), #64 was basically a random number for me. I'd experiment with bigger and smaller
+		keras.layers.Dropout(0.25),		# makes sure you aren't overfitting and killing your test accuracy. This is a pretty high dropout rate, so make lower as needed (esp if you need a bigger training set)
+		keras.layers.Dense(buildingOccupancy, activation=tf.nn.relu) #number of neurons in final layer should equal number of classes
+	])
+
+	model.load_weights("./checkpoints/weights")
+
+
+
 if __name__ == '__main__':
-	vis()
+	# predAlg(500)
+	# customAlg()
+	train(customAlg())
