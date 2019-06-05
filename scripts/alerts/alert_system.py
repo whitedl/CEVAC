f77155e3b810cbe6e666205b1af0ebb7b92c5bd7
Harrison Hall <harrisonchristianhall@gmail.com>
2019-06-05 11:08:28 -0400

    Alert script works
    This script can check databases for values and even compare temperatures with other databases. It cannot yet check for the times a sensor may be reporting.

--- scripts/alerts/alert_system.py
+++ scripts/alerts/alert_system.py
@@ -66,7 +66,9 @@ def angle_brackets_replace(regex_string,alert):
 	try:
 		regex_list = regex_string.replace("<","").split(">")
 		for regex in regex_list:
-			if regex == "" #TODO
+			if regex == "asdf":
+				#TODO
+				pass
 	except:
 		return regex_string
 
@@ -89,10 +91,11 @@ def import_conditions(fname,logger):
 						"day" : regex_to_numlist(row[COLUMNS["day"]]), #EST
 						"month" : regex_to_numlist(row[COLUMNS["month"]]), #EST
 						"condition" : row[COLUMNS["condition"]],
-						"value" : float(row[COLUMNS["value"]]),
+						"value" : row[COLUMNS["value"]],
 						"operation" : row[COLUMNS["operation"]],
 						"aliases" : alias_to_list(row[COLUMNS["aliases"]]),
 						"sort_column" : row[COLUMNS["sort_column"]],
+						"building" : row[COLUMNS["building"]],
 					}
 			except:
 				logger.error("Issue importing conditions "+str(i))
@@ -142,36 +145,117 @@ connection = pypyodbc.connect(
 cursor = connection.cursor()
 
 ## Check alerts
+total_issues = 0
 for i,a in enumerate(alerts):
 	alert = alerts[a]
+	insert_sql = "INSERT INTO CEVAC_ALL_ALERTS_HIST(AlertMessage,AlertType, Metric,BLDG,BeginTime) VALUES(?,?,?,?,GETUTCDATE())"
 	try:
 		now = datetime.datetime.now()
 		if (((str(now.isoweekday()) in alert["day"]) or (str(alert["day"]) == "*"))
 		and ((str(now.hour) in alert["hour"]) or (str(alert["hour"]) == "*"))
 		and ((str(now.month) in alert["month"]) or (str(alert["month"]) == "*"))):
-			selection_command = "SELECT top "+str(alert["num_entries"]) + " " + alert["column"] + " FROM " + str(alert["database"])
-			if str(alert["aliases"]) == "*":
-				selection_command += " ORDER BY " + alert["sort_column"] + " DESC"
+			# Check basic value
+			if str.isdigit(alert["value"]):
+				alert["value"] = float(alert["value"])
+
+				selection_command = "SELECT top "+str(alert["num_entries"]) + " " + alert["column"] + " FROM " + str(alert["database"])
+				if str(alert["aliases"]) == "*":
+					selection_command += " ORDER BY " + alert["sort_column"] + " DESC"
+				else:
+					selection_command += " WHERE " + "Alias" + " IN (" + str(alert["aliases"]).replace("[","").replace("]","") + ") ORDER BY " + alert["sort_column"] + " DESC"
+				data = cursor.execute(selection_command)
+				data_list = [row[0] for row in data]
+				avg_data = sum(data_list)/len(data_list)
+				send_alert = False
+				if alert["condition"] == ">":
+					send_alert = (avg_data > alert["value"])
+				elif alert["condition"] == "<":
+					send_alert = (avg_data < alert["value"])
+				if send_alert:
+					total_issues += 1
+					logging.info("ISSUE"+str(alert))
+					cursor.execute(insert_sql, [alert["message"],alert["operation"],avg_data,alert["building"]])
+					logging.info("An alert was sent for "+str(alert))
+				logging.info("Checked "+str(alert))
+
+			# Temperature custom measure
+			elif ("Temp" in alert["value"]):
+				selection_command = "SELECT Alias, " + alert["column"] + " FROM " + alert["database"] + " ORDER BY " + alert["sort_column"]
+				data = cursor.execute(selection_command)
+				data_list = [[row[0],row[1]] for row in data]
+				temps = {}
+				for row in data_list:
+					room = row[0].split()[0]
+					if room in temps:
+						temps[room][row[0][row[0].find(" ")+1:]] = float(row[1])
+					else:
+						temps[room] = {
+							row[0][row[0].find(" ")+1:] : float(row[1])
+						}
+
+				for room in temps:
+					Alias_Temp = "Temp"
+					for key in temps[room].keys():
+						if (key != "Cooling SP" and key != "Heating SP"):
+							Alias_Temp = key
+					# Modify value
+					room_vals = temps[room]
+					try:
+						if "+" in alert["value"].split()[-1]:
+							val_str = alert["value"].split()[-1]
+							val = float(val_str[val_str.find("+")+1:])
+							room_vals["Cooling SP"] += val
+							room_val["Heating SP"] += val
+						elif "-" in alert["value"].split()[-1]:
+							val_str = alert["value"].split()[-1]
+							val = float(val_str[val_str.find("-")+1:])
+							room_vals["Cooling SP"] -= val
+							room_vals["Heating SP"] -= val
+					except:
+						pass
+
+					# Check value
+					send_alert = False
+
+					if ">" in alert["condition"]:
+						if "Cooling SP" in alert["value"]:
+							if "Cooling SP" in room_vals:
+								send_alert = (room_vals["Cooling SP"] < room_vals[Alias_Temp])
+						if "Heating SP" in alert["value"]:
+							if "Heating SP" in room_vals:
+								send_alert = (room_vals["Heating SP"] < room_vals[Alias_Temp])
+					elif "<" in alert["condition"]:
+						if "Cooling SP" in alert["value"]:
+							if "Cooling SP" in room_vals:
+								send_alert = (room_vals["Cooling SP"] > room_vals[Alias_Temp])
+						if "Heating SP" in alert["value"]:
+							if "Heating SP" in room_vals:
+								send_alert = (room_vals["Heating SP"] > room_vals[Alias_Temp])
+
+					if send_alert:
+						total_issues += 1
+						logging.info("ISSUE"+str(alert))
+						cursor.execute(insert_sql, [alert["message"],alert["operation"],str(room) + " " + str(room_vals[Alias_Temp]),alert["building"]])
+						logging.info("An alert was sent for "+str(alert))
+					logging.info("Checked "+str(alert))
+
+			# Time custom measure
+			elif ("<now>" in alert["value"]):
+				#local_dt = local.localize(naive, is_dst=None)
+				#utc_dt = local_dt.astimezone(pytz.utc)
+				logging.error("<now> not yet ready in script")
+
 			else:
-				selection_command += " WHERE " + "Alias" + " IN (" + str(alert["aliases"]).replace("[","").replace("]","") + ") ORDER BY " + alert["sort_column"] + " DESC"
-			data = cursor.execute(selection_command)
-			data_list = [row[0] for row in data]
-			avg_data = sum(data_list)/len(data_list)
-			send_alert = False
-			if alert["condition"] == ">":
-				send_alert = (avg_data > alert["value"])
-			elif alert["condition"] == "<":
-				send_alert = (avg_data < alert["value"])
-			if send_alert:
-				insert_sql = "INSERT INTO CEVAC_ALL_ALERTS_HIST(AlertMessage,AlertType, Metric,BLDG,BeginTime) VALUES(?,?,?,?,GETUTCDATE())"
-				cursor.execute(insert_sql, [alert["message"],alert["operation"],avg_data,alert["building"]])
-				logging.info("An alert was sent for "+str(alert))
-			logging.info("Checked "+str(alert))
+				logging.error("Could not find valid condition/value for "+str(alert))
 		else:
-			#print("Not time for",alert)
-			pass
+			logging.info(alert["alert_name"]+"Not time")
 	except:
 		logging.error("Issue on alert "+str(i)+" "+str(alert))
+
+if total_issues == 0:
+	insert_sql = "INSERT INTO CEVAC_ALL_ALERTS_HIST(AlertMessage,AlertType, Metric,BLDG,BeginTime) VALUES('All Clear','N/A','','All',GETUTCDATE())"
+else:
+	logging.info(str(datetime.datetime.now())+" TOTAL ISSUES: "+str(total_issues)
 
 
 logging.shutdown()
