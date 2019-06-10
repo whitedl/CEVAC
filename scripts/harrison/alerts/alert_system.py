56d567c8a54a6c29409fc88a601a98c846b64038
bmeares <bennett.meares@gmail.com>
2019-06-10 11:44:00 -0400

    retab

--- scripts/harrison/alerts/alert_system.py
+++ scripts/harrison/alerts/alert_system.py
@@ -200,7 +200,7 @@ for i,a in enumerate(alerts):
                                 print(url_command)
                                 print(selection_command)
                                 data = urllib.request.urlopen(command_to_query(selection_command)).read().replace("}{","} {").split(" ")
-                                                                print(data)
+                                print(data)
                                 dict_list = [json.loads(d) for d in data]
                                                                 print(dict_list)
                                 data_list = [sd[list(sd.keys())[0]] for sd in dict_list]
