1b7d1cb9224bd0e73e8b109a244b9ea62c0cebeb
Bennett Meares <bmeares@inscribe.productions>
2019-06-11 19:59:37 -0400

    transfer

--- scripts/bmeares/Bash/append_tables.sh
+++ scripts/bmeares/Bash/append_tables.sh
@@ -2,7 +2,13 @@
 
 /home/bmeares/scripts/seperator.sh
 echo Appending tables...
-tables="CEVAC_WATT_TEMP_HIST_VIEW,CEVAC_WATT_POWER_HIST_VIEW,CEVAC_WATT_IAQ_HIST_VIEW,CEVAC_WATT_POWER_SUMS_HIST_VIEW"
+tables="CEVAC_WATT_TEMP_HIST_VIEW,"
+tables=$tables"CEVAC_WATT_POWER_HIST_VIEW,"
+tables=$tables"CEVAC_WATT_IAQ_HIST_VIEW,"
+tables=$tables"CEVAC_WATT_POWER_SUMS_HIST_VIEW,"
+tables=$tables"CEVAC_ASC_IAQ_HIST_VIEW,"
+tables=$tables"CEVAC_ASC_TEMP_HIST_VIEW,"
+tables=$tables"CEVAC_LEE_III_TEMP_HIST_VIEW"
 
 
 /home/bmeares/scripts/exec_sql.sh "EXEC CEVAC_CACHE_APPEND @tables = '$tables'"
