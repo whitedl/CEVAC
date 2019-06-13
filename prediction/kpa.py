c9543fb8d745f92354ba4531c05bacf536898987
Andrew <abemery@clemson.edu>
2019-06-13 09:36:05 -0400

    6 layers, all 512 except for final. all relu except for final [softmax]; 1250 epochs should land right at acc = .35; will test after lunch using those weights

--- prediction/kpa.py
+++ prediction/kpa.py
@@ -2,12 +2,14 @@ from __future__ import absolute_import, division, print_function
 
 # TensorFlow and tf.keras
 import keras
+# from tensorflow.keras.callbacks import EarlyStopping
 from keras import losses
 from keras.utils import plot_model
 from keras.layers import Dense, Activation, Dropout, Conv1D
 
 # Helper libraries
 import numpy as np
+from numpy import array
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -15,6 +17,7 @@ import pandas as pd
 import scipy
 import datetime
 import os
+import random
 
 # read in our data
 def loadData():
@@ -37,9 +40,52 @@ def predAlg(numClasses):
 
 	# create the keras instance
     model = keras.Sequential()
-    model.add(Dense(512, input_dim=6))
-    model.add(Activation('elu'))
-    model.add(Dropout(0.3))
+    model.add(Dense(512, input_shape=(6,)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.35))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(300))
+    model.add(Activation('softmax'))
+
+	# if changes are going to be made to increase accuracy it should be done here
+    model.compile(optimizer='adam',loss=losses.categorical_crossentropy, metrics=['accuracy'])
+
+    return model
+
+# trains the model
+def train(model):
+
+    # load data
+    train_data, train_labels, test_data, test_labels = loadData()
+
+    # stops the model when the loss is no longer decreasing
+    # early_stopping = EarlyStopping(monitor='loss', patience=15)
+
+    #more epochs = more work training ~= higher accuracy
+    model.fit(train_data, train_labels, epochs=1250, verbose=1) # , callbacks=[early_stopping]
+
+    # for making re-running faster, toggle this to re-run with the same weights from the previous run
+    model.save_weights('powerModel.h5')
+    # model.load_weights("./checkpoints/weights")	# works in reverse)
+
+    test_loss, test_acc = model.evaluate(test_data, test_labels)
+
+    print('Test accuracy:', test_acc)
+
+# loads weights and makes a prediction
+def predict(inq):
+    # make a model instanace
+    model = keras.Sequential()
+    model.add(Dense(512, input_shape=(6,)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.35))
     model.add(Dense(512))
     model.add(Activation('relu'))
     model.add(Dense(512))
@@ -58,42 +104,41 @@ def predAlg(numClasses):
     model.add(Activation('softmax'))
 
 	# if changes are going to be made to increase accuracy it should be done here
-    model.compile(optimizer='adam',loss=losses.categorical_crossentropy, metrics=['accuracy'])
-
-    return model
-
-# trains the model
-def train(model):
-
-	# load data
-	train_data, train_labels, test_data, test_labels = loadData()
-
-	# stops the model when the loss is no longer decreasing
-	# early_stopping = EarlyStopping(monitor='loss', patience=100)
-
-	#more epochs = more work training ~= higher accuracy
-	model.fit(train_data, train_labels, epochs=10000, verbose=1) # , callbacks=[early_stopping]
-
-	# for making re-running faster, toggle this to re-run with the same weights from the previous run
-	model.save_weights('powerModel.h5')
-	# model.load_weights("./checkpoints/weights")	# works in reverse)
-
-	test_loss, test_acc = model.evaluate(test_data, test_labels)
-
-	print('Test accuracy:', test_acc)
-
-# loads weights and makes a prediction
-def predict(inq):
-    # make a model instanace
-    model = keras.Sequential([
-		keras.layers.Dense(512, input_shape=(3,), activation=tf.nn.relu), #64 was basically a random number for me. I'd experiment with bigger and smaller
-		keras.layers.Dropout(0.5),		# makes sure you aren't overfitting and killing your test accuracy. This is a pretty high dropout rate, so make lower as needed (esp if you need a bigger training set)
-		keras.layers.Dense(300, activation=tf.nn.relu) #number of neurons in final layer should equal number of classes
-	])
-    model.compile(optimizer='sgd',loss=losses.categorical_crossentropy, metrics=['accuracy'])
+    # model.compile(optimizer='adam',loss=losses.categorical_crossentropy, metrics=['accuracy'])
     model.load_weights('powerModel.h5')
-    model.predict(inq)
+    x = model.predict(inq)
+    choice = 0
+    num = -1
+    for i, ran in enumerate(x[0]):
+        if ran > choice:
+            choice = ran
+            num = i
+    return num
+
+# calculates and displays accuracy
+def disp():
+    results = []
+    testingData = np.load('powerTestingData.npy')
+    testingLabels = np.load('powerTestingLabels.npy')
+    for index, element in enumerate(testingData):
+        y = testingLabels[index]
+        for ind, el in enumerate(y):
+            if el == 1:
+                yIndex = ind
+        y = yIndex
+        prediction = predict(element.reshape(1,-1))
+        err = (((prediction - y) / y)**2)**.5 * 100
+        results.append(err)
+        print('ERROR:\t{}\t\tPREDICTION:\t{}'.format(int(err), prediction))
+    return(results)
 
 if __name__ == '__main__':
 	train(predAlg(300))
-    # predict([10, 10, 5])
+    # disp()
+
+# [[ 78.     1.     6.03  12.    21.     2.  ]
+#  [ 50.     1.    12.49  11.    19.     2.  ]
+#  [100.    90.     8.04  12.    18.     4.  ]
+#  ...
+#  [ 72.    75.    23.    11.    15.     3.  ]
+#  [ 47.     1.    14.    11.    13.     4.  ]
