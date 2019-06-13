ffb529817ea83a28a6da9300cec4f091df6b4b7a
Bennett Meares <bmeares@inscribe.productions>
2019-06-13 14:22:59 -0400

    update

---
+++ scripts/bmeares/Bash/lasr_xref_driver.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+/home/bmeares/scripts/lasr.sh WATT POWER XREF
+/home/bmeares/scripts/lasr.sh WATT WAP XREF
+/home/bmeares/scripts/lasr.sh WATT TEMP XREF
+/home/bmeares/scripts/lasr.sh WATT IAQ XREF
+/home/bmeares/scripts/lasr.sh WATT WATER XREF
+
+
+/home/bmeares/scripts/lasr.sh ASC IAQ XREF
+/home/bmeares/scripts/lasr.sh ASC TEMP XREF
+
+/home/bmeares/scripts/lasr.sh LEE_III TEMP XREF
+
+
+
