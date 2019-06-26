a4cd24977269d2aa65689ed9c5ef2515861fc356
bmeares <bennett.meares@gmail.com>
2019-06-26 11:08:56 -0400

    update

--- scripts/bmeares/Bash/exec_sql.sh
+++ scripts/bmeares/Bash/exec_sql.sh
@@ -4,18 +4,29 @@ if [ -z "$1" ]; then
   echo Error: Missing SQL query
   exit 1
 fi
-
-query=$1
+query="$1"
 h='130.127.218.11'
 u='wficcm'
 db='WFIC-CEVAC'
 p='5wattcevacmaint$'
 
+if [ ! -z "$2" ]; then
+  output="$2"
+  echo "Outputting to /home/bmeares/cache/$output"
+  if [[ "$query" != *"NOCOUNT"*  ]]; then
+    query="
+    SET NOCOUNT ON
+    $query
+    "
+  fi
+  echo $'Executing query:\n\n'"$query"
+  # Get columns and data
+  /opt/mssql-tools/bin/sqlcmd -S $h -U $u -d $db -P $p -Q "$query" -W -o "/home/bmeares/cache/temp_$output" -s"," -w 700
+  # remove separator
+  cat /home/bmeares/cache/temp_$output | sed 2d > /home/bmeares/cache/$output && rm -f /home/bmeares/cache/temp_$output
+else
+  echo $'Executing query:\n\n'"$query"
+  /opt/mssql-tools/bin/sqlcmd -S $h -U $u -d $db -P $p -Q "$query" -W -w 700
+fi
 
-# output=$(sqlcmd -S 130.127.218.11 -U wficcm -d WFIC-CEVAC -P 5wattcevacmaint$ -Q "$sum_query")
-
-echo $'Executing query:\n\n'$query
-
-raw=$(/opt/mssql-tools/bin/sqlcmd -S $h -U $u -d $db -P $p -Q "$query")
-echo "$raw"
 
