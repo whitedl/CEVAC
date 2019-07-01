415aa4b63dcdaa19513d94563043c0a2b61d83fc
Drew Emery <abemery@clemson.edu>
2019-07-01 15:34:22 -0400

    for bennett

--- prediction/predictor.py
+++ prediction/predictor.py
@@ -15,36 +15,50 @@ from sys import argv
 import requests
 import json
 import time
+import datetime
 from datetime import date
 import numpy as np
+import pytz
 # import matplotlib as plt
 
 # fetch our weather forecast
 def fetch():
-    # json format of the hourly data
+
     hourly = {
-                'hours' : [],
-                'days' : [],
-                'months' : [],
-                'years' : [],
-                'humidities' : [],
-                'temperatures' : [],
-                'clouds' : []
-                }
+            'hours' : [],
+            'weekdayNames' : [],
+            'monthNames' : [],
+            'seconds' : [],
+            'weekNumbers' : [],
+            'minutes' : [],
+            'months' : [],
+            'days' : [],
+            'years' : [],
+            'humidities' : [],
+            'temperatures' : [],
+            'clouds' : []
+            }
 
     requestURL = 'https://api.darksky.net/forecast/db6bb38a65d59c7677e8e97db002705b/33.662333,-79.830875'
     r = requests.get(requestURL).json()
     hourlyData = r['hourly']['data']
+    print(hourlyData[0])
 
     # insert every element in the hourly data
     for element in hourlyData:
         hourly['hours'].append(int(time.strftime('%H', time.localtime(element['time']))))
+        hourly['weekdayNames'].append(str(time.strftime('%a', time.localtime(element['time']))))
+        hourly['monthNames'].append(str(time.strftime('%b', time.localtime(element['time']))))
+        hourly['seconds'].append(str(time.strftime('%S', time.localtime(element['time']))))
+        hourly['weekNumbers'].append(str(time.strftime('%W', time.localtime(element['time']))))
+        hourly['minutes'].append(str(time.strftime('%M', time.localtime(element['time']))))
         hourly['months'].append(int(time.strftime('%m', time.localtime(element['time']))))
         hourly['days'].append(int(time.strftime('%d', time.localtime(element['time']))))
         hourly['years'].append(int(time.strftime('%Y', time.localtime(element['time']))))
         hourly['humidities'].append(element['humidity'])
         hourly['temperatures'].append(((element['temperature'] - 32) / 1.8 + 20) / 70)
         hourly['clouds'].append(element['cloudCover'])
+
     return hourly
 
 # normalize the hours, days, and months
@@ -103,6 +117,8 @@ def pred(model):
 
     for i, hour in enumerate(hourly['hours']):
 
+
+
         day = hourly['days'][i]
         month = hourly['months'][i]
         year = hourly['years'][i]
@@ -118,13 +134,54 @@ def pred(model):
         prediction = model.predict(input.reshape(1,-1))[0][0] * 275
         predictions.append(prediction)
 
-    str = ''
-    for i, element in enumerate(predictions):
 
-        str += 'ESTIMATE {} ON {}/{} AT HOUR {}\n'.format(element, hourly['days'][i], hourly['months'][i], hourly['hours'][i])
-    with open('predictions.txt', 'w') as f:
-        f.write(str)
-    return predictions
+    insert_sql_total = ''
+
+    for i, prediction in enumerate(predictions):
+            m = hourly['months'][i]
+            d = (hourly['days'][i] - 1) % 7
+            Y = hourly['years'][i]
+            H = hourly['hours'][i]
+
+            weekdayName = hourly['weekdayNames'][i]
+            day = hourly['days'][i]
+            monthName = hourly['monthNames'][i]
+            second = hourly['seconds'][i]
+            weekNumber = hourly['weekNumbers'][i]
+            minute = hourly['minutes'][i]
+            month = hourly['months'][i]
+
+            hms = ':'.join((str(H), str(minute), str(second)))
+            datestring = ' '.join((weekdayName, monthName, str(day)))
+
+            datestring = datestring + ' ' + hms + ' ' + str(Y)
+
+            # datestring = ' '.join((weekdayName, monthName, str(day), ':'.join((str(H), str(minute), str(second)), str(Y))))
+
+            t = datetime.datetime.strptime(f"{m} {d} {Y} {H}","%m %w %Y %H")
+
+            ETDateTime = t.strftime("%Y-%m-%d %H:%M:%S")
+
+            dst = False
+            local = pytz.timezone ("America/New_York")
+            naive = datetime.datetime.strptime(datestring, "%a %b %d %H:%M:%S %Y")
+            local_dt = local.localize(naive, is_dst=dst)
+            t_utc = local_dt.astimezone (pytz.utc)
+            UTCDateTime = t_utc.strftime("%Y-%m-%d %H:%M:%S")
+
+            insert_sql_total += ("INSERT INTO CEVAC_WATT_POWER_SUMS_PRED_HIST_RAW "
+                                 "(UTCDateTime, ETDateTime, Total_Usage) "
+                                 f" VALUES({UTCDateTime},{ETDateTime},"
+                                 f"{str(prediction)});")
+
+    # Write to `CEVAC_WATT_POWER_SUMS_PRED_HIST`
+    f = open("/home/bmeares/cache/insert_predictions.sql", "w")
+    f.write(insert_sql_total.replace(';', '\nGO\n'))
+    f.close()
+    os.system("/CEVAC/scripts/exec_sql_script.sh "
+              "/CEVAC/cache/insert_predictions.sql")
+    os.remove("/CEVAC/cache/insert_predictions.sql")
+
 
 if __name__ == '__main__':
     model = createModel()
