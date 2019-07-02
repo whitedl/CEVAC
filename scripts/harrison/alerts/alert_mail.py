e294168c15cc1f3c7ee57c487e49160d32f9c1c4
Harrison Hall <harrisonchristianhall@gmail.com>
2019-07-02 12:53:07 -0400

    test

--- scripts/harrison/alerts/alert_mail.py
+++ scripts/harrison/alerts/alert_mail.py
@@ -5,11 +5,17 @@ import datetime
 import time_handler
 import smtplib
 import ssl
+from jinja2 import Template
 
 
 email = "cevac5733@gmail.com"
 password = "cevacsteve5733"
-to_list = ["hchall@g.clemson.edu"]  # , "bmeares@g.clemson.edu"]
+to_list = {
+    "Harrison Hall": "hchall@g.clemson.edu",
+    # "Bennett Meares": "bmeares@g.clemson.edu"
+}
+f = open("alert_email.html", "r")
+page = Template("".join(f.readlines()))
 
 
 def email_message(email, password, to_list, message, subject):
@@ -25,8 +31,11 @@ def email_message(email, password, to_list, message, subject):
         server.login(email, password)
         print(email, subject, "\n", message)
         for person in to_list:
+            email = to_list[person]
+            p_page = Template.render(Name=email)
+            print(p_page)
             message = f"Subject: {subject}\n\n{message}"
-            server.sendmail(email, person, message)
+            # server.sendmail(email, person, message)
 
 
 def main():
@@ -37,8 +46,9 @@ def main():
     yesterday = now - day
     now_str = time_handler.sql_time_str(now)
     yesterday_str = time_handler.sql_time_str(yesterday)
-    alerts = bsql.Query(f"SELECT TOP 20 * FROM CEVAC_ALL_ALERTS_HIST WHERE "
-                        f"UTCDateTime BETWEEN '{yesterday_str}' AND "
+    print(now_str, yesterday_str)
+    alerts = bsql.Query(f"SELECT TOP 20 * FROM CEVAC_ALL_ALERTS_HIST_RAW WHERE"
+                        f" UTCDateTime BETWEEN '{yesterday_str}' AND "
                         f"'{now_str}' ORDER BY UTCDateTime DESC")
     now_etc = time_handler.utc_to_est(now)
     yesterday_etc = time_handler.utc_to_est(yesterday)
@@ -54,7 +64,7 @@ def main():
         message = alert[2].strip()
         metric = alert[3].strip()
         building = alert[4].strip()
-        acknowledged = bool(int(alert[5]))
+        acknowledged = bool(int(alert[6]))
         etc = alert[8].strip()
 
         e_msg = ""
