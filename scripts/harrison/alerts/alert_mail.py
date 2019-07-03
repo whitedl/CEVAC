4906dbac6d33e52fb888f0d9fe8df82120b8ef34
Harrison Hall <harrisonchristianhall@gmail.com>
2019-07-03 18:30:57 -0400

    actually cron-ready

--- scripts/harrison/alerts/alert_mail.py
+++ scripts/harrison/alerts/alert_mail.py
@@ -14,12 +14,12 @@ email = "cevac5733@gmail.com"
 password = "cevacsteve5733"
 to_list = {
     "Harrison Hall": "hchall@g.clemson.edu",
-    # "Bennett Meares": "bmeares@g.clemson.edu",
+    "Bennett Meares": "bmeares@g.clemson.edu",
     # "Inscribe boi": "bmeares@inscribe.productions",
-    # "Zach Smith": "ztsmith@g.clemson.edu",
+    "Zach Smith": "ztsmith@g.clemson.edu",
     # "Zach Klein": "ztklein@g.clemson.edu",
     # "Drewboi": "abemery@clemson.edu",
-    # "Tim": "timh@clemson.edu",
+    "Tim": "timh@clemson.edu",
 }
 f = open("alerts/alert_email.html", "r")
 page = Template("".join(f.readlines()))
