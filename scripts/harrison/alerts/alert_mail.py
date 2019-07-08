66d1c2b383ed6b0ddf08d7c5a3bb0232e9b4a9e7
Harrison Hall <harrisonchristianhall@gmail.com>
2019-07-08 15:24:56 -0400

    re-ready for re-cronning

--- scripts/harrison/alerts/alert_mail.py
+++ scripts/harrison/alerts/alert_mail.py
@@ -6,34 +6,23 @@ import datetime
 import time_handler
 import smtplib
 import ssl
-import base64
 from jinja2 import Template
 from email import message as msg
-
-
-def encode_img(img_file):
-    """Encode html image into base64."""
-    with open(img_file, "rb") as image:
-        encoded_string = base64.b64encode(image.read())
-        return encoded_string
 
 
 email = "cevac5733@gmail.com"
 password = "cevacsteve5733"
 to_list = {
     "Harrison Hall": "hchall@g.clemson.edu",
-<<<<<<< HEAD
     "Bennett Meares": "bmeares@g.clemson.edu",
     "Inscribe boi": "bmeares@inscribe.productions",
     #  "Zach Smith": "ztsmith@g.clemson.edu",
-=======
     # "Bennett Meares": "bmeares@g.clemson.edu",
     # "Inscribe boi": "bmeares@inscribe.productions",
     # "Zach Smith": "ztsmith@g.clemson.edu",
->>>>>>> fb052c7646d78c06275a297589ddb700cffff2fb
     # "Zach Klein": "ztklein@g.clemson.edu",
     # "Drewboi": "abemery@clemson.edu",
-    # "Tim": "timh@clemson.edu",
+    # "Tim Howard": "timh@clemson.edu",
 }
 f = open("/cevac/CEVAC/scripts/harrison/alerts/alert_email.html", "r")
 page = Template("".join(f.readlines()))
@@ -66,33 +55,32 @@ old_metrics = {
     }
 }
 
-sz = 10
 pic_path = "/cevac/DEV/scripts/harrison/alerts/pics/"
 metrics = {
     "TEMP": {
         "key": "<TEMP>",
-        "char": (f"<img src=\"{encode_img(pic_path+'TEMP.png')}"
-                 f" width=\"{sz}%\" height=\"{sz}%\">"),
+        "char": (f"<img src=\"https://i.imgur.com/7idtl34.png\""
+                 f" width=\"50\" height=\"50\">"),
     },
     "POWER": {
         "key": "<POWER>",
-        "char": (f"<img src=\"\""
-                 f" width=\"{sz}%\" height=\"{sz}%\">"),
+        "char": (f"<img src=\"https://i.imgur.com/8dxzfpX.png\""
+                 f" width=\"50\" height=\"50\">"),
     },
     "IAQ": {
         "key": "<IAQ>",
-        "char": (f"<img src=\"\""
-                 f" width=\"{sz}%\" height=\"{sz}%\">"),
+        "char": (f"<img src=\"https://i.imgur.com/vkFWgSf.png\""
+                 f" width=\"50\" height=\"50\">"),
     },
     "CHW": {
         "key": "<CHW>",
-        "char": (f"<img src=\"\""
-                 f" width=\"{sz}%\" height=\"{sz}%\">"),
+        "char": (f"<img src=\"https://i.imgur.com/OtTAHcl.png\""
+                 f" width=\"50\" height=\"50\">"),
     },
     "STEAM": {
         "key": "<STEAM>",
-        "char": (f"<img src=\"\""
-                 f" width=\"{sz}%\" height=\"{sz}%\">"),
+        "char": (f"<img src=\"https://i.imgur.com/GdXwxMy.png\""
+                 f" width=\"50\" height=\"50\">"),
     },
 
     "UNKNOWN": {
@@ -171,7 +159,8 @@ def email_message(email, password, to_list, message, subject):
         server.login(email, password)
         for person in to_list:
             p_email = to_list[person]
-            p_page = page.render(Name=person, message=message, subject=subject)
+            p_page = page.render(Name=person, message=message, subject=subject,
+                                 metrics=metrics)
 
             m_message = msg.Message()
             m_message.add_header('Content-Type', 'text/html')
