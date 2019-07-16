5d86c1670cb768a659a0adc68494fa49db51f7ff
Drew Emery <abemery@clemson.edu>
2019-07-16 16:16:22 -0400

    added some comments for clarity; tim says that code reviews are on the horizon and this scares me

--- prediction/dataAnalysis.py
+++ prediction/dataAnalysis.py
@@ -7,12 +7,15 @@ class analyst(object):
     def __init__(self, df):
         self.data = pd.read_csv(df, error_bad_lines=False)
 
+    # print the
     def yieldMax(self, attr):
         print(a.data[attr].max())
 
+    # graph two attributes of the data next to each other
     def graph(self, xatt, yatt):
         yield None
 
+    # search the dataframe for rows that satisfy predefined metrics
     def searchdf(self, attr, val):
         self.attr = attr
         self.val = val
@@ -20,15 +23,15 @@ class analyst(object):
             print(name)
         classes = []
         for index, row in self.data.iterrows():
-            # if row['summary'] not in classes:
-            #     classes.append(row['summary'])
             if row['summary'] == 'Clear' and int(row['time'][-2:]) > 8 and int(row['time'][-2:]) < 22 and row['apparentTemperature'] > 90:
                 print(row)
-        # print(classes)
+
+    # show all of the column names
     def showCategories(self):
         for columnName in self.data:
             print(columnName)
 
+    # show all of the classes that a column has
     def showClasses(self, column):
         classes = []
         for index, row in self.data.iterrows():
