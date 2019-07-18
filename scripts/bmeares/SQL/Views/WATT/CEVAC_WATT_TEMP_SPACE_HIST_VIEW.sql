71973c7ba28540faa84bf17503d0a25c64caf5ff
Bennett Meares <bennett.meares@gmail.com>
2019-07-18 16:40:52 -0400

    housecleaning

--- scripts/bmeares/SQL/Views/WATT/CEVAC_WATT_TEMP_SPACE_HIST_VIEW.sql
+++
@@ -1,7 +0,0 @@
-WITH aliases AS (
-	SELECT Alias FROM CEVAC_WATT_TEMP_XREF
-	WHERE RoomType IN (
-		'Breakroom','Classroom','Comm Studio','Kitchen','Office','Project Room'
-	) AND ReadingType = 'Temp'
-) SELECT * FROM CEVAC_WATT_TEMP_LATEST AS l WHERE l.Alias IN (SELECT * FROM aliases);
-
--- scripts/bmeares/SQL/Views/WATT/insert_building_emergency_xref.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO CEVAC_WATT_POWER_XREF(PointSliceID, ObjectName, BLG, Floor, Type, Reading, Alias)
-VALUES (-1, 'SUM_OF_EMERGENCIES', 'Watt', 'Building', 'Emergency', 'Live KW', 'Building Emergency')
