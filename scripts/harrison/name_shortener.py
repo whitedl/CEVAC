2743e24e7523f6db248c9cecf858ae58e4e7e68b
Harrison Hall <harrisonchristianhall@gmail.com>
2019-07-23 13:59:40 -0400

    name shortener working

--- scripts/harrison/name_shortener.py
+++ scripts/harrison/name_shortener.py
@@ -7,16 +7,23 @@ table = argv[-1]
 
 name_to_shortened = {
     "CEVAC": "C",
-    "POWER": "PO",
-    "SUMS": "SU",
-    "COMPARE": "CO",
-    "LATEST": "LA",
-    "BROKEN": "BR",
-    "LASR": "LASR",
+
+    "POWER": "POW",
+    "TEMP": "TEM",
+    "IAQ": "IAQ",
+    "CO2": "CO2",
+
+    "SUMS": "SUM",
+    "COMPARE": "COM",
+    "LATEST": "LAT",
+    "BROKEN": "BRO",
+    "LASR": "",
 }
 
 words = table.split("_")
 for i, word in enumerate(words):
     if word.upper() in name_to_shortened:
         words[i] = name_to_shortened[word.upper()]
+    elif i > 1:
+        words[i] = words[i][:3]
 print("_".join(words))
