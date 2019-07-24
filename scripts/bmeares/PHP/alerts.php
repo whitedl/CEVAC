7a21fd7c43209a5c81f0bee04e2b76849399e54c
bmeares <bennett.meares@gmail.com>
2019-07-24 13:30:31 -0400

    bugfixes

--- scripts/bmeares/PHP/alerts.php
+++ scripts/bmeares/PHP/alerts.php
@@ -11,8 +11,7 @@ if(count($_GET) > 0) $where = substr($where, 0, -3);
 else $where = "";
 
 $query = "
-SELECT * FROM
-CEVAC_ALL_ALERTS_EVENTS_HIST
+SELECT * FROM CEVAC_ALL_ALERTS_EVENTS_HIST
 $where
 ";
 
