49f616e4f95a6a689e257eaceb4548538fc8c2b1
ZTKlein <ztklein@clemson.edu>
2019-08-05 16:57:58 -0400

    Building now has hasMany relation with Statistic

--- Web_API/src/repositories/building.repository.ts
+++ Web_API/src/repositories/building.repository.ts
@@ -1,14 +1,32 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Building, BuildingRelations} from '../models';
+import {
+  DefaultCrudRepository,
+  HasManyRepositoryFactory,
+  repository,
+} from '@loopback/repository';
+import {Building, BuildingRelations, Statistic} from '../models';
+import {StatisticRepository} from './statistic.repository';
 import {MssqlDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import {inject, Getter} from '@loopback/core';
 
 export class BuildingRepository extends DefaultCrudRepository<
   Building,
   typeof Building.prototype.buildingsname,
   BuildingRelations
 > {
-  constructor(@inject('datasources.mssql') dataSource: MssqlDataSource) {
+  public readonly statistics: HasManyRepositoryFactory<
+    Statistic,
+    typeof Building.prototype.buildingsname
+  >;
+
+  constructor(
+    @inject('datasources.mssql') dataSource: MssqlDataSource,
+    @repository.getter('StatisticRepository')
+    protected statisticRepositoryGetter: Getter<StatisticRepository>,
+  ) {
     super(Building, dataSource);
+    this.statistics = this.createHasManyRepositoryFactoryFor(
+      'statistics',
+      statisticRepositoryGetter,
+    );
   }
 }
