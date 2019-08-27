b6378984574c73147a6d8bb83105f1cf739343cc
bmeares <bennett.meares@gmail.com>
2019-08-27 15:10:26 -0400

    changed scripts to reflect autoCAHE and autoLASR

--- scripts/bmeares/Bash/lasr_master.sh
+++ scripts/bmeares/Bash/lasr_master.sh
@@ -4,12 +4,12 @@
 /cevac/scripts/lock.sh
 
 echo "Updating HIST"
-if ! /cevac/scripts/lasr_hist_driver.sh ; then
+if ! /cevac/scripts/lasr_hist_driver.sh "0" "norun" "append" "HIST"; then
   /cevac/scripts/log_error.sh "Failed updating HIST tables"
   exit 1
 fi
 echo "Updating LATEST"
-if ! /cevac/scripts/lasr_latest_driver.sh ; then
+if ! /cevac/scripts/lasr_hist_driver.sh "0" "norun" "append" "LATEST"; then
   /cevac/scripts/log_error.sh "Failed updating LATEST tables"
   exit 1
 fi
