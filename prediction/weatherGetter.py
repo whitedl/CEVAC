7fa91f478e667717f21dc3d1a977d4a8623010c9
Drew Emery <abemery@clemson.edu>
2019-09-02 14:11:10 -0400

    something broken with weathergetter; limiting the amount of entries to historicweather.csv

--- prediction/weatherGetter.py
+++ prediction/weatherGetter.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import csv
 
 # list being written to historicWeather.csv
-historicWeather = [["time", 'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature', 'dewPoint', 'humidity', 'cloudCover', 'uvIndex', 'visibility']]
+historicWeather = [['time', 'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature', 'dewPoint', 'humidity', 'cloudCover', 'uvIndex', 'visibility']]
 
 with open('api.json') as f:
     account = json.load(f)
@@ -13,11 +13,15 @@ with open('api.json') as f:
 apiKey = account['key']
 
 def fetch(days):
-    t = str(int(time.time() - 86400 * days))
-    url = 'https://api.darksky.net/forecast/' + apiKey + '/33.662333,-79.830875' + ',' + t
-    r = requests.get(url).json()
-    data = r['hourly']['data']
-    appendData(data)
+    try:
+        t = str(int(time.time() - 86400 * days))
+        url = 'https://api.darksky.net/forecast/' + apiKey + '/33.662333,-79.830875' + ',' + t
+        r = requests.get(url).json()
+        data = r['hourly']['data']
+        appendData(data)
+    except:
+        print('EXCEPTION')
+        pass
 
 def appendData(data):
 
@@ -26,13 +30,14 @@ def appendData(data):
         # create a temporary list to append to the historic data
         temp = []
 
-        for key in ["time", 'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature', 'dewPoint', 'humidity', 'cloudCover', 'uvIndex', 'visibility']:
+        for key in ["time", 'precipIntensity', 'precipProbability', 'temperature', 'apparentTemperature', 'humidity', 'cloudCover', 'uvIndex', 'visibility']:
             if key == 'time':
                 temp.append(time.strftime('%Y-%B-%d %H', time.localtime(element['time'])))
             else:
                 try:
                     temp.append(element[key])
                 except:
+                    print('NULL ENTRY')
                     temp.append('NULL')
 
         # add to historic weather
@@ -47,6 +52,6 @@ def writeData():
 
 if __name__ == '__main__':
 
-    for i in range(500, 0, -1):
+    for i in range(40, 0, -1):
         fetch(i)
     writeData()
