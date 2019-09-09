2f9785b45b69eab5bffb9c553b46aaec22191bb7
bmeares <bennett.meares@gmail.com>
2019-09-09 12:28:04 -0400

    conflicts

--- scripts/harrison/wap/cooper/daily_count.py
+++ scripts/harrison/wap/cooper/daily_count.py
@@ -8,8 +8,6 @@ import datetime
 from datetime import datetime as dt
 import csv
 import logging
-import urllib.request
-import urllib.parse
 
 # Setup configuration
 SEND = True
@@ -24,7 +22,6 @@ database_name = "CEVAC_COOPER_WAP_DAILY_HIST_RAW"
 CLIENT = 0
 MAC = 2
 SSID = 7
-
 
 
 # Script
@@ -77,7 +74,7 @@ for file in yesterdays_files:
 
     except Exception:
         errors += 1
-        logging.error("Could not parse file " + str(file))
+        logging.error("Could not parse file (formatting issue) " + str(file))
 
 # Push to database
 eduroam = 0 if "eduroam" not in network else len(network["eduroam"])
@@ -101,7 +98,6 @@ logging.info("clemson_count: " + str(eduroam))
 logging.info("guest_count: " + str(clemsonguest))
 
 if SEND:
-    # urllib.request.urlopen(command_to_query(insert_sql_total)).read()
     f = open("/cevac/cache/insert_daily_wap2.sql", "w")
     f.write(insert_sql_total.replace(';', '\nGO\n'))
     f.close()
@@ -112,7 +108,7 @@ else:
     print(insert_sql_total)
 
 if errors == 0:
-    logging.info("Successfully inserted into daily database")
+    logging.info("Successfully inserted into daily database.")
 logging.shutdown()
 
 """
