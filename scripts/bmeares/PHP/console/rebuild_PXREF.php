e5184f6d28d7d18e1d32cafc8edab0d6bbcb329f
bmeares <bennett.meares@gmail.com>
2019-09-09 12:26:38 -0400

    fixed bug

---
+++ scripts/bmeares/PHP/console/rebuild_PXREF.php
@@ -0,0 +1,16 @@
+<?php
+include "../functions.php";
+
+$BuildingSName = $_POST['BuildingSName'];
+$Metric = $_POST['Metric'];
+$unitOfMeasureID = $_POST['unitOfMeasureID'];
+$key_words = $_POST['key_words'];
+
+$script = '/cevac/scripts/CREATE_VIEW.sh';
+$config = "";
+$exec = "$script $BuildingSName $Metric PXREF";
+
+echo "$exec";
+echo `$exec`;
+
+?>
--- scripts/bmeares/PHP/manage.php
+++ scripts/bmeares/PHP/manage.php
@@ -7,6 +7,7 @@
     <meta charset="utf-8" />
     <title>CEVAC Database Management</title>
     <link rel="stylesheet" type="text/css" href="css/har.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
     <link rel="stylesheet" type="text/css" href="css/slider.css">
     <link rel="stylesheet" type="text/css" href="css/radio.css">
     <script src="console/JS/console.js"></script>
@@ -28,35 +29,38 @@
        <?php echo buildings_html(); ?>
         <select name="Metric" id="metrics" style="display: none"></select><br>
 
-        <div id="autoCACHE_div" style="display:none">
-          <h4>autoCACHE</h4>
-          <label for="autoCACHE" id="autoCACHE_label" class="switch">
-            <input type="checkbox" name="attributes[]" value="autoCACHE" id='autoCACHE' onclick="toggle()">
-            <span class="slider round"></span>
-          </label>
-        </div>
-        <div id="autoLASR_div" style="display:none">
-          <h4>autoLASR</h4>
-          <label for="autoLASR" id="autoLASR_label" class="switch">
-            <input type="checkbox" name="attributes[]" value="autoLASR" id='autoLASR' onclick="toggle()">
-            <span class="slider round"></span>
-          </label>
-        </div>
-
-          <div id="advanced_right" class="advanced">
-            <label for="custom_metric_name">New Metric</label>
-            <input type="text" name="custom_metric_name" id="custom_metric_name">
-            <label for="keys_list">Keywords</label>
-            <input type="text" name="keys_list" id="keys_list">
-            <label for=unitOfMeasureID"">unitOfMeasureID</label>
-            <input type="number" name="unitOfMeasureID" id="unitOfMeasureID">
-
+        <div id="toggles_div">
+          <div id="autoCACHE_div">
+            <h4>autoCACHE</h4>
+            <label for="autoCACHE" id="autoCACHE_label" class="switch">
+              <input type="checkbox" name="attributes[]" value="autoCACHE" id='autoCACHE' onclick="toggle()">
+              <span class="slider round"></span>
+            </label>
           </div>
+          <div id="autoLASR_div">
+            <h4>autoLASR</h4>
+            <label for="autoLASR" id="autoLASR_label" class="switch">
+              <input type="checkbox" name="attributes[]" value="autoLASR" id='autoLASR' onclick="toggle()">
+              <span class="slider round"></span>
+            </label>
+          </div>
+        </div>
+
+        <div id="advanced_right" class="advanced" style="visibility: hidden">
+          <label for="custom_metric_name">New Metric</label>
+          <input type="text" name="custom_metric_name" id="custom_metric_name">
+          <label for="keys_list">Keywords</label>
+          <input type="text" name="keys_list" id="keys_list">
+          <label for=unitOfMeasureID"">unitOfMeasureID</label>
+          <input type="number" name="unitOfMeasureID" id="unitOfMeasureID">
+
+        </div>
       </form>
       <br>
       <div id="buttons_parent_div">
-        <div id='advanced_div' class="advanced">
-          <button name="PXREF_button" id="PXREF_button" onclick="get_PXREF_html()">View PXREF</button><br>
+        <div id='advanced_div' class="advanced" style="visibility: hidden">
+          <button name="PXREF_button" id="PXREF_button" onclick="PXREF_button()">View PXREF</button><br>
+          <button name="rebuild_PXREF_button" id="rebuild_PXREF_button" onclick="rebuild_PXREF()">Rebuild PXREF</button><br>
         </div>
         <div id="buttons_div" style="display:none">
           <button name="bootstrap_button" id="bootstrap_button" onclick="bootstrap()">Bootstrap</button>
