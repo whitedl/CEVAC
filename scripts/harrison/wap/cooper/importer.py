771824ffdbba2a5b618d3c9b4a2060b0329a6681
Harrison Hall <harrisonchristianhall@gmail.com>
2019-09-17 15:23:04 -0400

    Tabs/spaces issue fix

--- scripts/harrison/wap/cooper/importer.py
+++ scripts/harrison/wap/cooper/importer.py
@@ -33,17 +33,17 @@ SEND = True
 # Function Definitions
 #######################################
 def safe_move(old_path, new_path):
-	try:
-		os.rename(old_path, new_path)
-	except WindowsError as e:
-		ext = new_path.rfind('.')
-		timestring = str(time.time()).replace(".", "_")
-		if(ext != -1):
-			new_path = new_path[0:ext] + '(' + timestring + ')' + new_path[ext:len(new_path)]
-			os.rename(old_path, new_path)
-		else:
-			new_path = new_path + '(' + timestring + ')'
-			os.rename(old_path, new_path)
+    try:
+        os.rename(old_path, new_path)
+    except WindowsError as e:
+        ext = new_path.rfind('.')
+        timestring = str(time.time()).replace(".", "_")
+        if(ext != -1):
+            new_path = new_path[0:ext] + '(' + timestring + ')' + new_path[ext:len(new_path)]
+            os.rename(old_path, new_path)
+        else:
+            new_path = new_path + '(' + timestring + ')'
+            os.rename(old_path, new_path)
 
 
 def custom_datestring_to_datetime(datestring):
