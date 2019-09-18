b93b62d2f705f7398293e53e5843ed4f3ebb6bb7
Drew Emery <abemery@clemson.edu>
2019-09-18 13:03:25 -0400

    here you go bennett sorry i'm dumn :D

--- prediction/validate.py
+++ prediction/validate.py
@@ -4,11 +4,11 @@ import pandas as pd
 # load the data and the labels
 data = np.load('powerTrainingData.npy')
 labels = np.load('powerTrainingLabels.npy')
-#
-# for d in data:
-#     print(d)
-#     for entry in d:
-#         if entry > 1:
-#             print(entry)
-l = list(map(tuple, np.where(np.isnan(data))))
-print(l)
+
+# find the nans in numpy array
+def findNAN(dataset):
+    locations = list(map(tuple, np.where(np.isnan(data))))
+    return locations
+
+if __name__ == '__main__':
+    print(findNan(data))
