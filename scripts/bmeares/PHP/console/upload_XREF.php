6526927ed05dfc6737af60fc65ca94976a64a811
bmeares <bennett.meares@gmail.com>
2019-09-18 13:03:51 -0400

    commit

--- scripts/bmeares/PHP/console/upload_XREF.php
+++ scripts/bmeares/PHP/console/upload_XREF.php
@@ -6,11 +6,11 @@ $Metric = $_POST['Metric'];
 $unitOfMeasureID = $_POST['unitOfMeasureID'];
 $key_words = $_POST['key_words'];
 
-$script = '/cevac/scripts/CREATE_VIEW.sh';
+$script = '/cevac/python/CREATE_VIEW.sh';
 $config = "";
 $exec = "$script $BuildingSName $Metric PXREF";
 
 echo "$exec";
-echo `$exec`;
+// echo `$exec`;
 
 ?>
