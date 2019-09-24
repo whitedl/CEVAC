f92dd2ae5dfc6e98f0fae6ed6db10e85b11d3b99
Bennett Meares <bennett.meares@gmail.com>
2019-09-24 15:21:12 -0400

    Update CEVAC_WATT_POWER_HIST_VIEW.sql

--- scripts/bmeares/SQL/Views/CUSTOM_DEFS/CEVAC_WATT_POWER_HIST_VIEW.sql
+++ scripts/bmeares/SQL/Views/CUSTOM_DEFS/CEVAC_WATT_POWER_HIST_VIEW.sql
@@ -5,6 +5,7 @@ WITH CEVAC_WATT_POWER_EMERGENCY_HIST AS (
 	INNER JOIN CEVAC_WATT_POWER_RAW_QUORUM AS q ON q.agg_key = x.ReadingType AND h.UTCDateTime >= q.begin_UTC AND h.UTCDateTime < end_UTC
 	WHERE x.ReadingType = 'Emergency'
 	GROUP BY UTCDateTime
+	HAVING q.PSID_count = COUNT(ActualValue)
 ),
 CEVAC_WATT_POWER_ISOLATED_GROUND_HIST AS (
 	SELECT -2 AS PointSliceID, 'Building Isolated Ground' AS Alias, UTCDateTime, SUM(ActualValue) AS ActualValue 
@@ -13,6 +14,7 @@ CEVAC_WATT_POWER_ISOLATED_GROUND_HIST AS (
 	INNER JOIN CEVAC_WATT_POWER_RAW_QUORUM AS q ON q.agg_key = x.ReadingType AND h.UTCDateTime >= q.begin_UTC AND h.UTCDateTime < end_UTC
 	WHERE x.ReadingType = 'Isolated Ground'
 	GROUP BY UTCDateTime
+	HAVING q.PSID_count = COUNT(ActualValue)
 ),
 CEVAC_WATT_POWER_1ST_FLOOR_HIST AS (
 	SELECT -3 AS PointSliceID, '1st Floor Total' AS Alias, UTCDateTime, SUM(ActualValue) AS ActualValue 
@@ -21,6 +23,7 @@ CEVAC_WATT_POWER_1ST_FLOOR_HIST AS (
 	INNER JOIN CEVAC_WATT_POWER_RAW_QUORUM AS q ON q.agg_key = x.ReadingType AND h.UTCDateTime >= q.begin_UTC AND h.UTCDateTime < end_UTC
 	WHERE x.Floor = '1st Floor'
 	GROUP BY UTCDateTime
+	HAVING q.PSID_count = COUNT(ActualValue)
 ),
 CEVAC_WATT_POWER_2ND_FLOOR_HIST AS (
 	SELECT -4 AS PointSliceID, '2nd Floor Total' AS Alias, UTCDateTime, SUM(ActualValue) AS ActualValue 
@@ -29,6 +32,7 @@ CEVAC_WATT_POWER_2ND_FLOOR_HIST AS (
 	INNER JOIN CEVAC_WATT_POWER_RAW_QUORUM AS q ON q.agg_key = x.ReadingType AND h.UTCDateTime >= q.begin_UTC AND h.UTCDateTime < end_UTC
 	WHERE x.Floor = '2nd Floor'
 	GROUP BY UTCDateTime
+	HAVING q.PSID_count = COUNT(ActualValue)
 ),
 CEVAC_WATT_POWER_3RD_FLOOR_HIST AS (
 	SELECT -3 AS PointSliceID, '3rd Floor Total' AS Alias, UTCDateTime, SUM(ActualValue) AS ActualValue 
@@ -37,6 +41,7 @@ CEVAC_WATT_POWER_3RD_FLOOR_HIST AS (
 	INNER JOIN CEVAC_WATT_POWER_RAW_QUORUM AS q ON q.agg_key = x.ReadingType AND h.UTCDateTime >= q.begin_UTC AND h.UTCDateTime < end_UTC
 	WHERE x.Floor = '3rd Floor'
 	GROUP BY UTCDateTime
+	HAVING q.PSID_count = COUNT(ActualValue)
 ),
 CEVAC_WATT_POWER_4TH_FLOOR_HIST AS (
 	SELECT -3 AS PointSliceID, '4th Floor Total' AS Alias, UTCDateTime, SUM(ActualValue) AS ActualValue 
@@ -45,6 +50,7 @@ CEVAC_WATT_POWER_4TH_FLOOR_HIST AS (
 	INNER JOIN CEVAC_WATT_POWER_RAW_QUORUM AS q ON q.agg_key = x.ReadingType AND h.UTCDateTime >= q.begin_UTC AND h.UTCDateTime < end_UTC
 	WHERE x.Floor = '4th Floor'
 	GROUP BY UTCDateTime
+	HAVING q.PSID_count = COUNT(ActualValue)
 ),
 CEVAC_WATT_POWER_BASEMENT_HIST AS (
 	SELECT -3 AS PointSliceID, 'Basement Total' AS Alias, UTCDateTime, SUM(ActualValue) AS ActualValue 
@@ -53,6 +59,7 @@ CEVAC_WATT_POWER_BASEMENT_HIST AS (
 	INNER JOIN CEVAC_WATT_POWER_RAW_QUORUM AS q ON q.agg_key = x.ReadingType AND h.UTCDateTime >= q.begin_UTC AND h.UTCDateTime < end_UTC
 	WHERE x.Floor = 'Basement'
 	GROUP BY UTCDateTime
+	HAVING q.PSID_count = COUNT(ActualValue)
 )
 SELECT PointSliceID, Alias, UTCDateTime, dbo.ConvertUTCToLocal(UTCDateTime) AS 'ETDateTime', ActualValue FROM
 CEVAC_WATT_POWER_RAW_HIST
