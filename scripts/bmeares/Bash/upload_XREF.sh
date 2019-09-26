0f51a3234be29cc2197ae98fc502a167205612f3
bmeares <bennett.meares@gmail.com>
2019-09-26 15:39:56 -0400

    update

--- scripts/bmeares/Bash/upload_XREF.sh
+++ scripts/bmeares/Bash/upload_XREF.sh
@@ -7,11 +7,11 @@ fname="$1"
 
 # python3 -V
 echo "Calling python script..."
-# out=`/usr/bin/python3 /cevac/python/csv_process.py "$fname" 2>&1`
-# echo "$out"
-if ! python3 /cevac/python/csv_process.py "$fname" ; then
-  /cevac/scripts/log_error.sh "Failed to upload XREF"
-  exit 1
-fi
+out=`/usr/bin/python3 /cevac/python/csv_process.py "$fname" 2>&1`
+echo "$out"
+# if ! python3 /cevac/python/csv_process.py "$fname" ; then
+  # /cevac/scripts/log_error.sh "Failed to upload XREF"
+  # exit 1
+# fi
 
 /cevac/scripts/unlock.sh
