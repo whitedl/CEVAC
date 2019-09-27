a2a7f028f059d02ec6d2058b95c891c9855cf16d
Harrison Hall <harrisonchristianhall@gmail.com>
2019-09-27 14:35:31 -0400

    Import work orders

--- scripts/harrison/work_orders/import_work_orders.py
+++ scripts/harrison/work_orders/import_work_orders.py
@@ -3,23 +3,51 @@
 from pandas import read_excel, isna
 from os import listdir
 import datetime
+import time
+import os
+from stat import S_ISREG
 
 
 DEBUG = True
 SEND = False
 
 READ_DIR = "/home/hchall/"
+PROCESSED_DIR = "/home/hchall/"
+
+d = {'FIKE RECREATION CENTER ': 'FIKE',
+     'RIGGS HALL ': 'RIGGS',
+     'COOPER LIBRARY ': 'COOPER',
+     'LITTLEJOHN COLISEUM ': 'LITTLEJOHN',
+     'FLUOR DANIEL (ENGINEERING INNOVATION CENTER) EIB': 'FLUOR',
+     'ACADEMIC SUCCESS CENTER': 'ASC',
+     'LEE HALL ADDITION / LEE III': 'LEE_III',
+     'WATT FAMILY INNOVATION CENTER': 'WATT',
+     'MCCABE HALL ': 'MCCABE'}
 
 
 def list_to_comma_seperated_values(some_list):
     """Return list of comma seperated values."""
     csv = ""
     for i in some_list:
-        if i in ["NULL"]:
-            csv += f"{i},",
+        if i in ["NULL"]:  # No apostrophes on NULL
+            csv += f"{i},"
         else:
-            csv += f"'{i}',"
-    return csv[:-1]
+            csv += f"\'{i}\',"
+    return csv[:-1]  # Remove last comma
+
+
+def cleanup(some_dir):
+    """Remove files older than 2 weeks."""
+    now = time.time()
+    keep_period = 1209600  # 1209600 seconds == two weeks
+    cutoff = now - keep_period
+
+    for directory in [some_dir]:
+        for fname in os.listdir(directory):
+            fpath = os.path.join(directory, fname)
+            if (S_ISREG(os.stat(fpath).st_mode) and
+                    os.path.getatime(fpath) < cutoff):
+                os.remove(fpath)
 
 
 import_files = []
@@ -30,20 +58,24 @@ for file in listdir(READ_DIR):
 insert_sql_total = ""
 for file in import_files:
     document = read_excel(READ_DIR + file)
-    headers = [head for head in document]
-    print(headers)
+    headers = list(document.keys())  # list of headers
+    errors = 0
     for i in range(len(document)):
         try:
             values = ['']*len(headers)
             for j, header in enumerate(headers):
                 if isna(document[header][i]):
                     values[j] = "NULL"
+                elif j in [4]:
+                    values[j] = d.get(document[header][i], document[header][i])
                 elif j in [8, 14]:  # Date in format 31-JAN-19
                     some_date = datetime.datetime.strptime(document[header][i],
                                                            '%d-%b-%y')
                     values[j] = some_date.strftime('%Y-%m-%d %H:%M:%S')
                 else:
-                    values[j] = str(document[header][i])
+                    raw = str(document[header][i])
+                    modified = raw.replace("'", "''").strip(" ")
+                    values[j] = modified
 
             insert_sql = (f"INSERT INTO X ("
                           f"{list_to_comma_seperated_values(headers)}"
@@ -51,9 +83,21 @@ for file in import_files:
                           f"{list_to_comma_seperated_values(values)}"
                           f")\nGO\n")
             insert_sql_total += insert_sql
-        except Exception as e:
-            print(values)
-            print(e.traceback())
-            print(f"No data {Exception}")
+        except Exception:
+            print("ISSUE with value")
 
-    # print(insert_sql_total)
+
+if SEND:
+    f = open("/cevac/cache/insert_work_orders.sql", "w")
+    f.write(insert_sql_total.replace(';', '\nGO\n'))
+    f.close()
+    os.system("/cevac/scripts/exec_sql_script.sh "
+              "/cevac/cache/insert_work_orders.sql")
+    os.remove("/cevac/cache/insert_work_orders.sql")
+if DEBUG:
+    print(insert_sql_total)
+    print(f"Last Excel Length: {len(document)}")
+    print(f"Errors {errors}")
+
+if SEND and not DEBUG:
+    cleanup(PROCESSED_DIR)
