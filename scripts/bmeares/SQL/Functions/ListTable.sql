687ae4c7c44847c8b2e72b6201ee4142439059f3
Harrison Hall <harrisonchristianhall@gmail.com>
2019-10-04 12:58:57 -0400

    Move stuff

--- scripts/bmeares/SQL/Functions/ListTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION ListTable (@text NVARCHAR(300))
-RETURNS TABLE
-AS
-
-RETURN
-SELECT Split.a.value('.', 'NVARCHAR(MAX)') items
-FROM(SELECT CAST('<X>'+REPLACE(@text, ',', '</X><X>')+'</X>' AS XML) AS String) AS A
-CROSS APPLY String.nodes('/X') AS Split(a)
