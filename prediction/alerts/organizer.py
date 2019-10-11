f65e8b5b8af1ddd88a6f4b8279f25bad2871992b
Tyler Piel <44116860+tpiel@users.noreply.github.com>
2019-10-11 14:35:49 -0400

    Test sorting

--- prediction/alerts/organizer.py
+++ prediction/alerts/organizer.py
@@ -1,3 +1,18 @@
 import numpy as np
 import pandas as pd
 import string
+
+pdf=pd.read_csv('sample1.csv')
+
+# print(pdf.shape)
+# print(pdf.loc[:,'Metric'])
+
+for i, entry in pdf.iterrows():
+    if 'POWER' in entry['Metric']:
+
+        #Trying to sort out data from different metrics to get
+        #duration from parameters
+        if 'not reported' in entry['AlertMessage']:
+            print(entry['AlertID'])
+            duration = entry['AlertMessage'][-8:-6];
+            print(duration)
