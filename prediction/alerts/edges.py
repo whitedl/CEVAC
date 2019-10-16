ffc1f78d8deac355e8447053884af3854f56c3ff
Tyler Piel <44116860+tpiel@users.noreply.github.com>
2019-10-16 15:31:50 -0400

    Working on Screen Management

---
+++ prediction/alerts/edges.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import string
+
+#Goal: write a script to record connections between nodes and add weight
+# to them based on the frequency of connection
+
+#What kind of test set do we need for this task?
+    # - would it be an example of all alerts active, so all edges are present
+    # - could we set the weight of all edges in test set to zero
+    
