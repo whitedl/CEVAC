439cf33d4f7c9a0be08508f333fa47b4e7a1d9f7
Harrison Hall <harrisonchristianhall@gmail.com>
2019-10-18 15:29:25 -0400

    Alert system v2, work on alerts.py and email.py

---
+++ alerts/alert_system2/ml.py
@@ -0,0 +1,25 @@
+"""Machine learning algorithm."""
+
+
+class ML:
+    """ML suite for connected issues and finding root causes."""
+
+    def __init__(self):
+        """Initialize data and connections."""
+        pass
+
+    def add_nodes(self, alerts):
+        """Manage new nodes from alerts.
+
+        Add new nodes if alert is not a node.
+        Manage edge weights for current alerts.
+        """
+        pass
+
+    def send(self):
+        """Send nodes and edges back to database."""
+        pass
+
+    def queries(self):
+        """Execute necessary queries."""
+        pass
