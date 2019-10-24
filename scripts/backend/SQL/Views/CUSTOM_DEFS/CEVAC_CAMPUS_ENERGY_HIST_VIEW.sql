6f9c6079470f3d453477d87ca334025e0afaa2e2
Bennett Meares <bennett.meares@gmail.com>
2019-10-24 13:42:40 -0400

    Update CEVAC_CAMPUS_ENERGY_HIST_VIEW.sql

--- scripts/backend/SQL/Views/CUSTOM_DEFS/CEVAC_CAMPUS_ENERGY_HIST_VIEW.sql
+++ scripts/backend/SQL/Views/CUSTOM_DEFS/CEVAC_CAMPUS_ENERGY_HIST_VIEW.sql
@@ -3,8 +3,8 @@ WITH aliases AS (
 ),
 t_ids AS (
   SELECT *, DENSE_RANK() OVER(PARTITION BY Alias ORDER BY ETDateTime) AS T_ID FROM CEVAC_CAMPUS_ENERGY_HIST_RAW
-)
-SELECT t1.ETDateTime, t1.ActualValue, t1.ActualValue - t2.ActualValue AS Change, t1.Alias
-FROM t_ids AS t1
-INNER JOIN t_ids AS t2 ON t1.T_ID = t2.T_ID + 1 AND t1.Alias = t2.Alias
-
+), change AS (
+	SELECT t1.ETDateTime, t1.ActualValue, t1.ActualValue - t2.ActualValue AS Change, t2.ETDateTime AS 'BeginET', CAST(DATEDIFF(MINUTE, t2.ETDateTime, t1.ETDateTime) AS FLOAT) / CAST(60 AS FLOAT) AS 'hour_offset', t1.Alias
+	FROM t_ids AS t1
+	INNER JOIN t_ids AS t2 ON t1.T_ID = t2.T_ID + 1 AND t1.Alias = t2.Alias
+) SELECT *, (Change / hour_offset) AS 'Rate' FROM change
