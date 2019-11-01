3690b43f8bec880a22f0fa9bc0afbdb0374c6043
Tyler Piel <44116860+tpiel@users.noreply.github.com>
2019-11-01 15:29:20 -0400

    Updates

--- scripts/importers/tiling/autotiling2.py
+++ scripts/importers/tiling/autotiling2.py
@@ -13,8 +13,10 @@ from win32api import GetSystemMetrics
 from time import sleep
 import keyboard
 import win32gui
+import sys
 
-startapps = pywinauto.findwindows.find_elements(title_re=".*Dive")
+#Build Array 'apps' of windows and handles for all windows labeled "Dive"
+startapps = pywinauto.findwindows.find_elements(title_re=".*SAS")
 managed_window_handles = [child.handle for child in startapps]
 apps = []
 for handle in managed_window_handles:
@@ -27,13 +29,10 @@ for handle in managed_window_handles:
 screenx, screeny = (GetSystemMetrics(0), GetSystemMetrics(1))
 
 max_windows = 6
-
-#TO DO LIST:
-# Need to figure out keyboard pausing
+wait_time=5
 
 
-
-# Moves new windows in the following shapes:
+# Moves new windows in the following positions:
 all_positions = {
 
 
@@ -80,14 +79,15 @@ all_positions = {
         "h": screeny * (1/2),
         },
     }
+
 # Dict of positions
 keep_running = True
 while keep_running:
     keyboard.start_recording()
 
 
-
-    all_windows = pywinauto.findwindows.find_elements(title_re=".*Dive")
+    # Find "Dive" windows and add to apps and managed_window_handles
+    all_windows = pywinauto.findwindows.find_elements(title_re=".*SAS")
     for handle in list(set([spec.handle for spec in all_windows]) - set(managed_window_handles)):
         try:
             apps.append(application.Application().connect(handle=handle))
@@ -95,22 +95,30 @@ while keep_running:
         except Exception:
             pass
 
-    #Cycle Take 2
+#Cycling
+    #Focus 6 windows at a time (first 6 windows of apps)
     for i,app in enumerate(apps):
         if i<max_windows:
             apps[i].window(handle=managed_window_handles[i]).set_focus()
-    sleep(5)
+    sleep(wait_time)
 
+    # Cycle window positions based on max_windows
     for i,app in enumerate(apps):
         if i>=max_windows:
             apps[i],apps[i-max_windows]=(apps[i-max_windows],apps[i])
             managed_window_handles[i],managed_window_handles[i-max_windows]=(managed_window_handles[i-max_windows],managed_window_handles[i])
 
+    # Remove old windows
+    for i, handle in enumerate(managed_window_handles):
+        if handle not in [spec.handle for spec in pywinauto.findwindows.find_elements(title_re=".*SAS")]:
+            apps.remove(apps[i])
+            managed_window_handles.remove(managed_window_handles[i])
+
 
     # Tile windows correctly
     for i, (handle, app) in enumerate(zip(managed_window_handles, apps)):
         try:
-            positions = all_positions 
+            positions = all_positions
 
             if(i > max_windows):
                 i=i-max_windows
@@ -132,6 +140,8 @@ while keep_running:
     print(keys_pressed)
     nums = [str(val) for val in list(range(0, 10))]
     max_set = False
+
+    # Pause program
     if ("ctrl" in actual_keys and "space" in actual_keys):
         print("activated key")
         while not max_set:
@@ -140,10 +150,10 @@ while keep_running:
             keys_pressed = keyboard.stop_recording()
             actual_keys = [press.name for press in keys_pressed]
             for key in actual_keys:
-                if key in nums:
-                    max_windows = int(key) if int(key) > 0 else 10
-                    max_set = True
+                if key=="space":
+                    print("resume")
+                    max_set=True
                     break
                 elif key == "esc":
-                    keep_running = False
+                    sys.exit()
                     break
